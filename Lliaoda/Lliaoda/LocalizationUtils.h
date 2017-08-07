4215a4fa39830f6c5e378fd85b1052a59d566e31
liuxiang444 <719444625@qq.com>
2017-08-07 17:11:54 +0800

    tijiao

---
+++ Lliaoda/Lliaoda/LocalizationUtils.h
@@ -0,0 +1,16 @@
+//
+//  LocalizationUtils.h
+//  Checkme Mobile
+//
+//  Created by 李乾 on 15/3/5.
+//  Copyright (c) 2015年 VIATOM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalizationUtils : NSObject
+
+//处理默认语言
++ (NSString *)DPLocalizedString:(NSString *)translation_key;
+
+@end
---
+++ Lliaoda/Lliaoda/LocalizationUtils.m
@@ -0,0 +1,33 @@
+//
+//  LocalizationUtils.m
+//  Checkme Mobile
+//
+//  Created by 李乾 on 15/3/5.
+//  Copyright (c) 2015年 VIATOM. All rights reserved.
+//
+
+#import "LocalizationUtils.h"
+
+@implementation LocalizationUtils
+
+//处理默认语言
++ (NSString *)DPLocalizedString:(NSString *)translation_key {
+    
+    NSString * s = NSLocalizedString(translation_key, nil);
+    NSString *lang = [NSString stringWithFormat:@"%@",[LXUserDefaults objectForKey:Language]];
+    if (![lang isEqual:@"1"]) {
+        NSString * path = [[NSBundle mainBundle] pathForResource:@"Base" ofType:@"lproj"];
+        NSBundle * languageBundle = [NSBundle bundleWithPath:path];
+        s = [languageBundle localizedStringForKey:translation_key value:@"" table:nil];
+    }else{
+        NSString * path = [[NSBundle mainBundle] pathForResource:@"Base" ofType:@"lproj"];
+        NSBundle * languageBundle = [NSBundle bundleWithPath:path];
+        s = [languageBundle localizedStringForKey:translation_key value:@"" table:nil];
+    
+    }
+    
+   
+    return s;
+}
+
+@end
