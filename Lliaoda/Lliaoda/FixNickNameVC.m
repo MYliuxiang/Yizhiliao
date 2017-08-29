b61e951dda1597fc5fa0d6ca78cc368c2303a28b
小牛 <admin@xiaoniudeiMac.local>
2017-08-29 18:44:12 +0800

    2

--- Lliaoda/Lliaoda/FixNickNameVC.m
+++ Lliaoda/Lliaoda/FixNickNameVC.m
@@ -28,6 +28,8 @@
     self.bgView.layer.shadowRadius = 5.f;
     self.bgView.layer.shadowOpacity = .3f;
     self.bgView.layer.shadowOffset = CGSizeMake(0, 0);
+    
+    [self.textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
 }
 
 - (void)rightAction
@@ -70,7 +72,14 @@
             textField.text = [textField.text substringToIndex:10];
         }
 }
-
+//- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+//{
+//    if (textField == self.textField) {
+//        if (textField.text.length > 10) return NO;
+//    }
+//    
+//    return YES;
+//}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -79,6 +88,33 @@
 
 
 - (IBAction)saveButtonAC:(id)sender {
-    [self rightAction];
+//    [self rightAction];
+    NSDictionary *params;
+    params = @{@"domain":_textField.text};
+    [WXDataService requestAFWithURL:Url_account params:params httpMethod:@"POST" isHUD:YES isErrorHud:YES finishBlock:^(id result) {
+        if(result){
+            if ([[result objectForKey:@"result"] integerValue] == 0) {
+                
+                
+                self.model.nickname = _textField.text;
+                [self.navigationController popViewControllerAnimated:YES];
+                
+                
+                
+            }else{    //请求失败
+                [SVProgressHUD showErrorWithStatus:result[@"message"]];
+                dispatch_time_t delayTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC));
+                dispatch_after(delayTime, dispatch_get_main_queue(), ^{
+                    
+                    [SVProgressHUD dismiss];
+                });
+                
+            }
+        }
+        
+    } errorBlock:^(NSError *error) {
+        NSLog(@"%@",error);
+        
+    }];
 }
 @end
