654702349623773e46fa4c7c5eaeb8cbaa0d832d
小牛 <admin@xiaoniudeiMac.local>
2017-09-15 17:24:09 +0800

    2

--- Lliaoda/Lliaoda/AppDelegate.m
+++ Lliaoda/Lliaoda/AppDelegate.m
@@ -100,11 +100,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-//    if (![LXUserDefaults boolForKey:kIsFirstLauchApp]) {
-//    
-//        [self appconfig];
-//
-//    }
+    if (![LXUserDefaults boolForKey:kIsFirstLauchApp]) {
+    
+        [self appconfig];
+
+    }
     [LXUserDefaults setBool:YES forKey:kIsFirstLauchApp];
     [LXUserDefaults synchronize];
     
@@ -476,7 +476,7 @@
 
 - (void)dl_failedTransaction:(SKPaymentTransaction *)transaction {
     if(transaction.error.code != SKErrorPaymentCancelled) {
-        [SVProgressHUD showErrorWithStatus:LXSring(@" dispa")];
+        [SVProgressHUD showErrorWithStatus:LXSring(@"支付失敗")];
         dispatch_time_t delayTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC));
         dispatch_after(delayTime, dispatch_get_main_queue(), ^{
             
