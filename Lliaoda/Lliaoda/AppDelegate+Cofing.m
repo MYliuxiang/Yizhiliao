568fa6767d3759aba071fa191ac68fef5fcad573
liuxiang444 <719444625@qq.com>
2017-12-11 18:29:03 +0800

    2

--- Lliaoda/Lliaoda/AppDelegate+Cofing.m
+++ Lliaoda/Lliaoda/AppDelegate+Cofing.m
@@ -23,19 +23,7 @@
     
     agent = [NSString stringWithFormat:@"%@,%@,ios,%@,501",@"youliao",[infoDictionary objectForKey:@"CFBundleShortVersionString"],phoneVersion];
     mutableUrl = [[NSMutableString alloc] initWithString:[NSString stringWithFormat:@"https://liao.yizhiliao.live/api/%@",Url_appconfig]];
-    //    if ([lang hasPrefix:@"zh-Hant"]) {
-    //        agent = [NSString stringWithFormat:@"%@,%@,ios,%@,301",@"talktome",[infoDictionary objectForKey:@"CFBundleShortVersionString"],phoneVersion];
-    //        mutableUrl = [[NSMutableString alloc] initWithString:[NSString stringWithFormat:@"https://www.yizhiliao.tv/api/%@",Url_appconfig]];
-    //    }else
-//    if ([lang hasPrefix:@"id"]){
-//        agent = [NSString stringWithFormat:@"%@,%@,ios,%@,402",@"talktome",[infoDictionary objectForKey:@"CFBundleShortVersionString"],phoneVersion];
-//        mutableUrl = [[NSMutableString alloc] initWithString:[NSString stringWithFormat:@"https://www.yizhiliao.live/api/%@",Url_appconfig]];
-//    } else {
-//        agent = [NSString stringWithFormat:@"%@,%@,ios,%@,403",@"talktome",[infoDictionary objectForKey:@"CFBundleShortVersionString"],phoneVersion];
-//        mutableUrl = [[NSMutableString alloc] initWithString:[NSString stringWithFormat:@"https://www.yizhiliao.net/api/%@",Url_appconfig]];
-//    }
-//
-    
+   
     NSString *urlEnCode = [[mutableUrl substringToIndex:mutableUrl.length] stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
     NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlEnCode]];
     [urlRequest setAllHTTPHeaderFields:@{@"user-language":@"zh-tw",@"User-Agent":agent}];
